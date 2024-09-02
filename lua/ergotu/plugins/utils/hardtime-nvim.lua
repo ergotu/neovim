b15f0ff5d1d1162b56ff25d77db97cd75fa45101
Jordi Lambrechts <jordi@intigriti.com>
2024-09-02 15:11:48 +0200

    feat: add hardtime

---
+++ lua/ergotu/plugins/utils/hardtime-nvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    event = "LazyFile",
+    opts = {
+      disable_mouse = false,
+      disabled_filetypes = { "qf", "netrw", "neo-tree", "lazy", "mason", "oil" },
+    },
+  },
+}
