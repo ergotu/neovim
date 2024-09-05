3db17fc7b1d89957b071b04c1e1ae757eb003c0f
Jordi Lambrechts <jordi@intigriti.com>
2024-09-05 14:41:29 +0200

    fix(markdown): unknown function

--- lua/ergotu/lang/markdown.lua
+++ lua/ergotu/lang/markdown.lua
@@ -90,6 +90,7 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
       vim.fn["mkdp#util#install"]()
     end,
     keys = {
