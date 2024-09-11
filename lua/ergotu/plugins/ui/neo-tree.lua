b382fd9c3a119539984ba4adda526e872b859dbd
Jordi Lambrechts <jordi@intigriti.com>
2024-09-11 06:40:37 +0200

    chore: remove lazygit.nvim

--- lua/ergotu/plugins/ui/neo-tree.lua
+++ lua/ergotu/plugins/ui/neo-tree.lua
@@ -129,13 +129,5 @@ return {
         end
       end,
     })
-    vim.api.nvim_create_autocmd("TermClose", {
-      pattern = "*lazygit",
-      callback = function()
-        if package.loaded["neo-tree.sources.git_status"] then
-          require("neo-tree.sources.git_status").refresh()
-        end
-      end,
-    })
   end,
 }
