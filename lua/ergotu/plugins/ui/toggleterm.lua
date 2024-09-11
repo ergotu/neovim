36cfaec0bb5d8464bab543198a3b8f8bc8a621b2
Jordi Lambrechts <jordi@intigriti.com>
2024-09-11 07:39:07 +0200

    chore: remove horrible hack, it causes some annoying errors

--- lua/ergotu/plugins/ui/toggleterm.lua
+++ lua/ergotu/plugins/ui/toggleterm.lua
@@ -2,20 +2,5 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   cmd = "ToggleTerm",
-  opts = {
-    -- HACK: this makes the terminal open similiarily to how vscode does it, but it feels like this is kinda a shit way to do it
-    on_open = function(_)
-      local name = vim.fn.bufname("neo-tree")
-      local winnr = vim.fn.bufwinnr(name)
-
-      if winnr ~= -1 then
-        vim.defer_fn(function()
-          local cmd = string.format("Neotree toggle")
-          vim.cmd(cmd)
-          vim.cmd(cmd)
-          vim.cmd("wincmd p")
-        end, 100)
-      end
-    end,
-  },
+  opts = {},
 }
