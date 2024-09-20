3e83c062757197ea5930eb3d188e85af6bb944b4
Jordi Lambrechts <jordi@intigriti.com>
2024-09-20 07:43:45 +0200

    chore: move from vim.loop to vim.uv

--- lua/ergotu/init.lua
+++ lua/ergotu/init.lua
@@ -1,8 +1,8 @@
 -- The existence of the /etc/NIXOS file officializes that it is a NixOS partition
-vim.g.nix = vim.loop.fs_stat("/etc/NIXOS") and true or false
+vim.g.nix = vim.uv.fs_stat("/etc/NIXOS") and true or false
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
