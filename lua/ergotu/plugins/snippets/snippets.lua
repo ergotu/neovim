ed2fa84c3eeb186f47b87e805a104d72a184f28d
Jordi Lambrechts <jordi@intigriti.com>
2024-09-23 15:53:38 +0200

    chore(keymaps): remove redundant keymap

--- lua/ergotu/plugins/snippets/snippets.lua
+++ lua/ergotu/plugins/snippets/snippets.lua
@@ -21,24 +21,4 @@ return {
       table.insert(opts.sources, { name = "snippets" })
     end
   end,
-  keys = {
-    {
-      "<Tab>",
-      function()
-        return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-      end,
-      expr = true,
-      silent = true,
-      mode = { "i", "s" },
-    },
-    {
-      "<S-Tab>",
-      function()
-        return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
-      end,
-      expr = true,
-      silent = true,
-      mode = { "i", "s" },
-    },
-  },
 }
