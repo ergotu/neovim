d62cfc50400f5f3e0162825f40aeec0798e9c8ed
Jordi Lambrechts <jordi@intigriti.com>
2024-10-15 12:28:57 +0200

    fix(nvim-dap): properly get the icons from config

--- lua/ergotu/plugins/debugging/nvim-dap.lua
+++ lua/ergotu/plugins/debugging/nvim-dap.lua
@@ -51,7 +51,7 @@ return {
 
     vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
 
-    for name, sign in pairs(require("config").icons.dap) do
+    for name, sign in pairs(Util.config.icons.dap) do
       sign = type(sign) == "table" and sign or { sign }
       vim.fn.sign_define(
         "Dap" .. name,
