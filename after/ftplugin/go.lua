a6420b6708ef1bf1a66a68afb47e120d26a172f3
Jordi Lambrechts <jordi@intigriti.com>
2024-10-15 08:11:44 +0200

    feat(go): add GoModTidy command

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,3 +2,10 @@ local vo = vim.opt_local
 vo.tabstop = 4
 vo.shiftwidth = 4
 vo.softtabstop = 4
+
+vim.api.nvim_create_user_command("GoModTidy", function()
+  vim.cmd.LspStop()
+  vim.cmd("!go mod tidy -v")
+  vim.cmd.write()
+  vim.cmd.LspStart()
+end, {})
