631c8e96fa90d134629aea49f815298e0bcbbe22
Jordi Lambrechts <jordi@intigriti.com>
2024-10-23 13:27:51 +0200

    Revert 1 commits
    087c1ce 'feat(wezterm-types): add wezterm-types for my wezterm config'

--- lua/ergotu/lang/lua.lua
+++ lua/ergotu/lang/lua.lua
@@ -6,14 +6,11 @@ return {
     opts = {
       library = {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
   -- Manage libuv types with lazy. Plugin will never be loaded
   { "Bilal2453/luvit-meta", lazy = true },
-  -- Manage wezterm types with lazy. Plugin will never be loaded
-  { "gonstoll/wezterm-types", lazy = true },
   {
     "mfussenegger/nvim-lint",
     opts = {
