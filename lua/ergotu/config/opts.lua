b9f844a0a50002cba56db4dfbd1f585a97e3b026
Jordi Lambrechts <jordi@intigriti.com>
2024-11-08 07:54:41 +0100

    feat(opts): remove bigfile_size in favour of using snacks

--- lua/ergotu/config/opts.lua
+++ lua/ergotu/config/opts.lua
@@ -21,12 +21,6 @@ vim.g.statuscolumn = {
   folds_githl = true, -- highlight fold sign with git sign color
 }
 
--- Set filetype to `bigfile` for files larger than 1.5 MB
--- Only vim syntax will be enabled (with the correct filetype)
--- LSP, treesitter and other ft plugins will be disabled.
--- mini.animate will also be disabled.
-vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
-
 vim.g.trouble_lualine = false
 
 vim.g.use_markview = false
