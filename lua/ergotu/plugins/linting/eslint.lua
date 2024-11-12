9569ad9ca4a8a53e08258819e6489ed708c74eaa
Jordi Lambrechts <jordi@intigriti.com>
2024-11-12 06:33:50 +0100

    feat(eslint): add option to disable eslint autoformat

--- lua/ergotu/plugins/linting/eslint.lua
+++ lua/ergotu/plugins/linting/eslint.lua
@@ -1,3 +1,5 @@
+local autoformat = vim.g.eslint_autoformat == nil or vim.g.eslint_autoformat
+
 return {
   {
     "neovim/nvim-lspconfig",
@@ -7,6 +9,7 @@ return {
           settings = {
             -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
             workingDirectories = { mode = "auto" },
+            format = autoformat,
           },
         },
       },
