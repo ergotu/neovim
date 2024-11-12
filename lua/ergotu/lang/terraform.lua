25e280436c8e6df65a3dfe25563888634b74e137
Jordi Lambrechts <jordi@intigriti.com>
2024-11-12 06:22:25 +0100

    feat(terraform): add formatter for packer files

--- lua/ergotu/lang/terraform.lua
+++ lua/ergotu/lang/terraform.lua
@@ -31,6 +31,7 @@ return {
     optional = true,
     opts = {
       formatters_by_ft = {
+        hcl = { "packer_fmt" },
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
