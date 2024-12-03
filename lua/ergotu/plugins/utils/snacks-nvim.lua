c380b48ab2bd22c0ac7d9b90f5e43fcad4b7d114
Jordi Lambrechts <jordi@intigriti.com>
2024-12-03 06:10:46 +0100

    feat(snacks): add additional keymaps
    - Toggle scratch Buffer
    - Select scratch Buffer
    - notification history

--- lua/ergotu/plugins/utils/snacks-nvim.lua
+++ lua/ergotu/plugins/utils/snacks-nvim.lua
@@ -84,6 +84,27 @@ return {
     end,
     keys = {
       {
+        "<leader>.",
+        function()
+          Snacks.scratch()
+        end,
+        desc = "Toggle Scratch Buffer",
+      },
+      {
+        "<leader>S",
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = "Select Scratch Buffer",
+      },
+      {
+        "<leader>n",
+        function()
+          Snacks.notifier.show_history()
+        end,
+        desc = "Notification History",
+      },
+      {
         "<leader>un",
         function()
           Snacks.notifier.hide()
