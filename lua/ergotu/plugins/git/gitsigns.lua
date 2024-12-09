8450b22aa10ff0b57dc744048939effaaf825d33
Jordi Lambrechts <jordi@intigriti.com>
2024-12-09 06:24:37 +0100

    feat(gitsigns): add toggle keymaps

--- lua/ergotu/plugins/git/gitsigns.lua
+++ lua/ergotu/plugins/git/gitsigns.lua
@@ -116,4 +116,18 @@ return {
         }):map("<leader>gdw")
     end,
   },
+  {
+    "gitsigns.nvim",
+    opts = function()
+      Snacks.toggle({
+        name = "Git Signs",
+        get = function()
+          return require("gitsigns.config").config.signcolumn
+        end,
+        set = function(state)
+          require("gitsigns").toggle_signs(state)
+        end,
+      }):map("<leader>uG")
+    end,
+  },
 }
