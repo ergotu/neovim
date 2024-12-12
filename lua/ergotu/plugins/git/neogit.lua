3964f4339bf37e4ff76f12588554f1a1fd37b35a
Jordi Lambrechts <jordi@intigriti.com>
2024-12-12 07:13:53 +0100

    chore(neogit): clean up comments

--- lua/ergotu/plugins/git/neogit.lua
+++ lua/ergotu/plugins/git/neogit.lua
@@ -3,10 +3,8 @@ return {
     "NeogitOrg/neogit",
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
-      "sindrets/diffview.nvim", -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
     },
     cmd = "Neogit",
     keys = {
