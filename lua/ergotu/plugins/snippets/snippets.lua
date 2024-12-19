4cd3a725a598e3b43a867f46e5321c2249fbd6dd
Jordi Lambrechts <jordi@intigriti.com>
2024-12-19 09:44:48 +0100

    feat(blink): change blink config to work on main

--- lua/ergotu/plugins/snippets/snippets.lua
+++ lua/ergotu/plugins/snippets/snippets.lua
@@ -32,9 +32,7 @@ return {
     ---@type blink.cmp.Config
     opts = {
       sources = {
-        completion = {
-          enabled_providers = { "snippets" },
-        },
+        default = { "snippets" },
       },
     },
   },
