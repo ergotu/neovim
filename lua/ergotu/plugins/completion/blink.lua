3b248ad775aa2a7599678465ca01a48bfda5b9a7
Jordi Lambrechts <jordi@intigriti.com>
2024-12-19 10:30:46 +0100

    fix(blink): only override color symbol kind

--- lua/ergotu/plugins/completion/blink.lua
+++ lua/ergotu/plugins/completion/blink.lua
@@ -188,7 +188,9 @@ return {
     "saghen/blink.cmp",
     opts = function(_, opts)
       opts.appearance = opts.appearance or {}
-      opts.appearance.kind_icons = Util.config.icons.kinds
+      opts.appearance.kind_icons = vim.tbl_extend("keep", {
+        Color = "██", -- Use block instead of icon for color items to make swatches more usable
+      }, Util.config.icons.kinds)
     end,
   },
 }
