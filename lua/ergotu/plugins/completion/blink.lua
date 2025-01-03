6ee8a752fc71bd7d3230f3fa9466dbe6d37978b8
Jordi Lambrechts <jordi@intigriti.com>
2025-01-03 09:38:33 +0100

    refactor(blink): use the enter preset

--- lua/ergotu/plugins/completion/blink.lua
+++ lua/ergotu/plugins/completion/blink.lua
@@ -89,23 +89,8 @@ return {
         },
       },
       keymap = {
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-        ["<C-p>"] = { "select_prev", "fallback" },
-        ["<C-n>"] = { "select_next", "fallback" },
-
-        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<CR>"] = { "accept", "fallback" },
-
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<Esc>"] = { "hide", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
+        preset = "enter",
+        ["<C-y>"] = { "select_and_accept" },
       },
     },
     ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
