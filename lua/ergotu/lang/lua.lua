d4a371853a18a8e9e2a06dd5c8b9ebc245e6b32e
Jordi Lambrechts <jordi@intigriti.com>
2025-01-03 07:54:58 +0100

    feat(lang/lua): add additional config to lua_ls

--- lua/ergotu/lang/lua.lua
+++ lua/ergotu/lang/lua.lua
@@ -84,6 +84,37 @@ return {
           -- keys = {},
           settings = {
             Lua = {
+              misc = {
+                -- parameters = { "--loglevel=trace" },
+              },
+              hover = { expandAlias = false },
+              type = {
+                castNumberToInteger = true,
+                inferParamType = true,
+              },
+              diagnostics = {
+                disable = { "incomplete-signature-doc", "trailing-space", "no-unknown" },
+                -- enable = false,
+                groupSeverity = {
+                  strong = "Warning",
+                  strict = "Warning",
+                },
+                groupFileStatus = {
+                  ["ambiguity"] = "Opened",
+                  ["await"] = "Opened",
+                  ["codestyle"] = "None",
+                  ["duplicate"] = "Opened",
+                  ["global"] = "Opened",
+                  ["luadoc"] = "Opened",
+                  ["redefined"] = "Opened",
+                  ["strict"] = "Opened",
+                  ["strong"] = "Opened",
+                  ["type-check"] = "Opened",
+                  ["unbalanced"] = "Opened",
+                  ["unused"] = "Opened",
+                },
+                unusedLocalExclude = { "_*" },
+              },
               workspace = {
                 checkThirdParty = false,
               },
