2c1b7da1af1f86a29b7bdaa679138878e423be30
Jordi Lambrechts <jordi@intigriti.com>
2025-01-09 13:14:13 +0100

    refactor(blink): color swatch upstreamed

--- lua/ergotu/plugins/completion/blink.lua
+++ lua/ergotu/plugins/completion/blink.lua
@@ -203,9 +203,8 @@ return {
     "saghen/blink.cmp",
     opts = function(_, opts)
       opts.appearance = opts.appearance or {}
-      opts.appearance.kind_icons = vim.tbl_extend("keep", {
-        Color = "██", -- Use block instead of icon for color items to make swatches more usable
-      }, Util.config.icons.kinds)
+      opts.appearance.kind_icons =
+        vim.tbl_deep_extend("force", opts.appearance.kind_icons or {}, Util.config.icons.kinds)
     end,
   },
 }
