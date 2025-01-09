b1993a7ea7a6867026fc507c6d60473792c38958
Jordi Lambrechts <jordi@intigriti.com>
2025-01-09 09:27:54 +0100

    fix(keymaps): remove duplicate keymap

--- lua/ergotu/config/keymaps.lua
+++ lua/ergotu/config/keymaps.lua
@@ -174,7 +174,6 @@ Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
 Snacks.toggle.diagnostics():map("<leader>ud")
 Snacks.toggle.line_number():map("<leader>ul")
-Snacks.toggle.line_number():map("<leader>ul")
 Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 Snacks.toggle
   .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
