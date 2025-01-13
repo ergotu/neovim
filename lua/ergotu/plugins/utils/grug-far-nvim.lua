fd6a8f6cc88e0f68ff48cf2434e8a072144a1bd2
Jordi Lambrechts <jordi@intigriti.com>
2025-01-13 06:41:27 +0100

    feat(grug-far): add keymaps

--- lua/ergotu/plugins/utils/grug-far-nvim.lua
+++ lua/ergotu/plugins/utils/grug-far-nvim.lua
@@ -3,8 +3,9 @@ return {
   opts = { headerMaxWidth = 80 },
   cmd = "GrugFar",
   keys = {
+    { "<leader>sr", "", desc = "Search and Replace" },
     {
-      "<leader>sr",
+      "<leader>srR",
       function()
         local grug = require("grug-far")
         local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
@@ -17,6 +18,66 @@ return {
       end,
       mode = { "n", "v" },
       desc = "Search and Replace",
+    },
+    {
+      "<leader>srr",
+      function()
+        local grug = require("grug-far")
+        grug.open({
+          transient = true,
+          prefills = {
+            paths = vim.fn.expand("%"),
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace (Current File)",
+    },
+    {
+      "<leader>srw",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            search = vim.fn.expand("<cword>"),
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace (Word)",
+    },
+    {
+      "<leader>srW",
+      function()
+        local grug = require("grug-far")
+        grug.open({
+          transient = true,
+          prefills = {
+            paths = vim.fn.expand("%"),
+            search = vim.fn.expand("<cword>"),
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace (Word in Current File)",
+    },
+    {
+      "<leader>srw",
+      function()
+        local grug = require("grug-far")
+        grug.open({
+          transient = true,
+          prefills = {
+            paths = vim.fn.expand("%"),
+            search = vim.fn.expand("<cword>"),
+          },
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search and Replace (Word in Current File)",
     },
   },
 }
