35b585e9db29ec270c622924e8585b54df44f4e0
Jordi Lambrechts <jordi@intigriti.com>
2025-01-20 06:26:43 +0100

    feat(keymaps): add insert keymap to jump to closing element

--- lua/ergotu/config/keymaps.lua
+++ lua/ergotu/config/keymaps.lua
@@ -240,7 +240,26 @@ map("n", "dd", function()
   handle_yank_delete("dd")
 end, { noremap = true, silent = true })
 
--- duplicate line and comment out the original
-map("n", "yc", "yygccp")
-
 map("n", "<C-c>", "ciw")
+
+-- move over a closing element in insert mode
+map("i", "<C-l>", function()
+  local closers = { ")", "]", "}", ">", "'", '"', "`", "," }
+  local line = vim.api.nvim_get_current_line()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local after = line:sub(col + 1, -1)
+  local closer_col = #after + 1
+  local closer_i = nil
+  for i, closer in ipairs(closers) do
+    local cur_index, _ = after:find(closer)
+    if cur_index and (cur_index < closer_col) then
+      closer_col = cur_index
+      closer_i = i
+    end
+  end
+  if closer_i then
+    vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
+  else
+    vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+  end
+end, { desc = "move over a closing element" })
