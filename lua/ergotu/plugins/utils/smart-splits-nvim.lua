61ac78d33a71de8e3b7241190512f3e061acd927
Jordi Lambrechts <jordi@intigriti.com>
2025-02-12 12:17:21 +0100

    feat: make navigation easier on split kb

--- lua/ergotu/plugins/utils/smart-splits-nvim.lua
+++ lua/ergotu/plugins/utils/smart-splits-nvim.lua
@@ -34,26 +34,54 @@ return {
       {
         "<C-left>",
         function()
-          require("smart-splits").resize_left()
+          require("smart-splits").move_cursor_left()
         end,
         desc = "Go to Left Window",
       },
       {
         "<C-down>",
         function()
-          require("smart-splits").resize_down()
+          require("smart-splits").move_cursor_down()
         end,
         desc = "Go to Lower Window",
       },
       {
         "<C-up>",
         function()
-          require("smart-splits").resize_up()
+          require("smart-splits").move_cursor_up()
         end,
         desc = "Go to Upper Window",
       },
       {
         "<C-right>",
+        function()
+          require("smart-splits").move_cursor_right()
+        end,
+        desc = "Go to Right Window",
+      },
+      {
+        "<C-S-left>",
+        function()
+          require("smart-splits").resize_left()
+        end,
+        desc = "Go to Left Window",
+      },
+      {
+        "<C-S-down>",
+        function()
+          require("smart-splits").resize_down()
+        end,
+        desc = "Go to Lower Window",
+      },
+      {
+        "<C-S-up>",
+        function()
+          require("smart-splits").resize_up()
+        end,
+        desc = "Go to Upper Window",
+      },
+      {
+        "<C-S-right>",
         function()
           require("smart-splits").resize_right()
         end,
