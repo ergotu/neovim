8bbe103b09f4917437853fbb98d20121cbcd9719
ergotu <17287858+ergotu@users.noreply.github.com>
2025-05-02 12:34:29 +0200

    feat(zettelkasten): add presentation plugin

---
+++ lua/ergotu/plugins/zettelkasten/presentation.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "Chaitanyabsprip/present.nvim",
+    enabled = false,
+    opts = {
+      -- ... your config here
+    },
+  },
+  {
+    "sotte/presenting.nvim",
+    opts = {
+      -- fill in your options here
+      -- see :help Presenting.config
+    },
+    cmd = { "Presenting" },
+  },
+}
