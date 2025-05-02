c337b940f198029f7935322d27b6607619a7e502
ergotu <17287858+ergotu@users.noreply.github.com>
2025-05-02 13:03:34 +0200

    feat(eslint): provide customized handlers

--- lua/ergotu/plugins/linting/eslint.lua
+++ lua/ergotu/plugins/linting/eslint.lua
@@ -11,6 +11,27 @@ return {
             workingDirectories = { mode = "auto" },
             format = autoformat,
           },
+          before_init = function(params, config)
+            -- Set the workspace folder setting for correct search of tsconfig.json files etc.
+            config.settings.workspaceFolder = {
+              uri = params.rootPath,
+              name = vim.fn.fnamemodify(params.rootPath, ":t"),
+            }
+          end,
+          handlers = {
+            ["eslint/openDoc"] = function(_, params)
+              vim.ui.open(params.url)
+              return {}
+            end,
+            ["eslint/probeFailed"] = function()
+              vim.notify("LSP[eslint]: Probe failed.", vim.log.levels.WARN)
+              return {}
+            end,
+            ["eslint/noLibrary"] = function()
+              vim.notify("LSP[eslint]: Unable to load ESLint library.", vim.log.levels.WARN)
+              return {}
+            end,
+          },
         },
       },
     },
