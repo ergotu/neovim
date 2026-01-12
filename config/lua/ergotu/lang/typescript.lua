152e9ea83d1a502939a7e01a6e636872dd9e11e2
ergotu <17287858+ergotu@users.noreply.github.com>
2026-01-12 07:42:41 +0100

    chore(typescrip): add config for esling

--- config/lua/ergotu/lang/typescript.lua
+++ config/lua/ergotu/lang/typescript.lua
@@ -1,5 +1,34 @@
 local lsp = require("ergotu.config.lsp")
 
+lsp.add_server("eslint", {
+  settings = {
+    -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+    workingDirectories = { mode = "auto" },
+    format = vim.g.eslint_autoformat == nil or vim.g.eslint_autoformat,
+  },
+  before_init = function(params, config)
+    -- Set the workspace folder setting for correct search of tsconfig.json files etc.
+    config.settings.workspaceFolder = {
+      uri = params.rootPath,
+      name = vim.fn.fnamemodify(params.rootPath, ":t"),
+    }
+  end,
+  handlers = {
+    ["eslint/openDoc"] = function(_, params)
+      vim.ui.open(params.url)
+      return {}
+    end,
+    ["eslint/probeFailed"] = function()
+      vim.notify("LSP[eslint]: Probe failed.", vim.log.levels.WARN)
+      return {}
+    end,
+    ["eslint/noLibrary"] = function()
+      vim.notify("LSP[eslint]: Unable to load ESLint library.", vim.log.levels.WARN)
+      return {}
+    end,
+  },
+})
+
 -- Disable ts_ls in favor of vtsls
 lsp.add_server("ts_ls", {
   enabled = false,
