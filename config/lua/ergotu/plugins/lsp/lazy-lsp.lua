7d97f9d1f1996216a692ab3f9683c577e84ba05e
ergotu <17287858+ergotu@users.noreply.github.com>
2026-01-12 08:07:59 +0100

    chore(lazy-lsp): use curated server selection

--- config/lua/ergotu/plugins/lsp/lazy-lsp.lua
+++ config/lua/ergotu/plugins/lsp/lazy-lsp.lua
@@ -7,6 +7,22 @@ return {
     end,
     after = function()
       require("lazy-lsp").setup({
+        excluded_servers = {
+          "ccls", -- prefer clangd
+          "denols", -- prefer eslint and ts_ls
+          "docker_compose_language_service", -- yamlls should be enough?
+          "flow", -- prefer eslint and ts_ls
+          "ltex", -- grammar tool using too much CPU
+          "quick_lint_js", -- prefer eslint and ts_ls
+          "scry", -- archived on Jun 1, 2023
+          "tailwindcss", -- associates with too many filetypes
+          "biome", -- not mature enough to be default
+          "oxlint", -- prefer eslint
+        },
+        preferred_servers = {
+          markdown = {},
+          python = { "basedpyright", "ruff" },
+        },
         use_vim_lsp_config = true,
       })
     end,
